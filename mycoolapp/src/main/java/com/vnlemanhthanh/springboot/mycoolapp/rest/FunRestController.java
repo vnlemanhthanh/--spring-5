f9303c67a9386e1fa43f1b717b74984ceb68fb5e
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-10-06 15:41:23 +0700

    Spring Boot Introduction

---
+++ mycoolapp/src/main/java/com/vnlemanhthanh/springboot/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,16 @@
+package com.vnlemanhthanh.springboot.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+public class FunRestController {
+
+    @GetMapping({"/", "index.html"})
+    public String sayHello() {
+        return "Hello " + LocalDateTime.now();
+    }
+
+}
---
+++ mycoolapp/src/test/java/com/vnlemanhthanh/springboot/mycoolapp/MycoolappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vnlemanhthanh.springboot.mycoolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycoolappApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
